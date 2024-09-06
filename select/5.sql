c3c437ec62fea17057e010c358152b8ea318e954
anna <your@email.example>
2024-09-06 10:33:32 +0300

    select with like

---
+++ select/5.sql
@@ -0,0 +1,7 @@
+-- use sql_store;
+
+SELECT * FROM customers
+where last_name LIKE 'b%'
+-- where last_name LIKE '%e%'
+-- b_ _ _ _ ja vajag atrast kkadu noteikto simbolu skaitu, kkurs sakas piemeram ar b burtu
+ 
