78ed8b3bd23ede6033dff2afe4718f935625e65f
anna <your@email.example>
2024-09-06 09:20:43 +0300

    select for certain fields, custom fields + distinct

---
+++ select/2.sql
@@ -0,0 +1,10 @@
+-- uzdevums: atlasit datus no tabulas 'customers' 
+-- datubaze 'sql_store'
+-- atlasit vardu, uzvardu un punktus  
+
+-- use sql_store;
+
+-- select first_name, last_name, points, points+10 as "points_increased"
+-- from customers;
+
+SELECT DISTINCT state from customers; 
