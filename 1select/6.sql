ce8114f0439e7e764dafe4c0f88e8a1a478c95f3
anna <your@email.example>
2024-10-28 12:04:38 +0200

    sms

--- 1select/6.sql
+++ 1select/6.sql
@@ -4,12 +4,12 @@
 -- datubaazes 'employees' tabulas
 
  -- use sql_hr;
-	SELECT * FROM employees
+	-- SELECT * FROM employees
 
 -- 2. uzdevums 
 -- Atlasit visu klientu vardus no 'sql_store' datubazes 
 
--- use sql_store;
+use sql_store;
 -- SELECT first_name FROM customers
 
 -- 3.uzdevums
@@ -19,15 +19,15 @@
 
 -- use sql_hr
 -- SELECT * FROM employees
--- WHERE reports_to is not null
+ -- WHERE reports_to is  null
 
  -- 4. uzdevums
  -- Atlasi visus sutijumus no 'sql_store', kuru statuss
  -- ir "Processed" 
  
- -- SELECT 
--- 	order_id, customer_id, name
--- FROM
-   --  orders
--- join order_statuses
-	-- on orders.status = order_statuses.order_status_id;
+ SELECT 
+	order_id, customer_id, name
+FROM
+orders
+join order_statuses
+ on orders.status = order_statuses.order_status_id;
--- SMS.sql
+++ SMS.sql
@@ -1,5 +1,5 @@
--- CREATE DATABASE school_management_system;
--- USE school_management_system;
+CREATE DATABASE school_management_system;
+USE school_management_system;
 
 -- Skolas vadības sistēmas datubāze (School Management System)
 -- Izveidot datubāzi, kas reprezentē skolēnus, skolotājus,
@@ -15,38 +15,41 @@
 -- Ievadīt piecu skolēnu un viņu profilu datus. Izmantot “LAST_INSERT_ID()” funkciju
 -- Izveidot vaicājumu, lai atrastu visus skolēnus kopā ar viņu profiliem.
 
-CREATE TABLE Teachers (
-		teacher_id INT AUTO_INCREMENT PRIMARY KEY,
-		teacher_name VARCHAR(50) NOT NULL,
-		teacher_email VARCHAR(50),
-		department_id INT,
-		FOREIGN KEY (department_id) REFERENCES Departments(department_id));
+-- 3. uzdevums
+-- Katrs skolotājs var pasniegt vairākus kursus, bet katrs kurss ir saistīts ar tikai vienu skolotāju.
+-- Izveidot nepieciešamās datubāzes tabulas un saites, lai glabātu minētos datus.
+-- Ievadīt trīs skolotājus un katram piešķirt vairākus kursus.
+-- Izveidot vaicājumu, lai atrastu visus kursus, ko pasniedz noteikts skolotājs.
+
 
 CREATE TABLE Students (
 		student_id INT AUTO_INCREMENT PRIMARY KEY, 
 -- profile_id INT NOT NULL,
 		student_name VARCHAR(50) NOT NULL,
-		student_email VARCHAR(50),
-        FOREIGN KEY(profile_id) REFERENCES Student_Profiles(profile_id)
-        );
+		student_email VARCHAR(50)
+        ); 
+        
+CREATE TABLE Departments (
+		department_id INT AUTO_INCREMENT PRIMARY KEY,
+		department_name VARCHAR(50) NOT NULL);
         
 CREATE TABLE Courses (
 		course_id INT AUTO_INCREMENT PRIMARY KEY, 
         course_name VARCHAR(50) NOT NULL, 
-		department_id INT,
-		FOREIGN KEY (department_id) REFERENCES Departments(department_id));
+		teacher_id INT,
+		FOREIGN KEY (teacher_id) REFERENCES Teachers(teacher_id));
         
-	CREATE TABLE Departments (
-		department_id INT AUTO_INCREMENT PRIMARY KEY,
-		department_name VARCHAR(50) NOT NULL);
-    
-	CREATE TABLE Student_Profiles (
+CREATE TABLE Teachers (
+		teacher_id INT AUTO_INCREMENT PRIMARY KEY,
+		teacher_name VARCHAR(50) NOT NULL,
+		teacher_email VARCHAR(50));
+        
+CREATE TABLE Student_Profiles (
 	profile_id INT AUTO_INCREMENT PRIMARY KEY,
 	student_id INT,
 	address VARCHAR(50),
 	phone_number VARCHAR(50),
 	FOREIGN KEY (student_id) REFERENCES Students(student_id));
-    
     
 INSERT INTO Students(student_name, student_email)
 VALUES ('Janis Berzins', 'janis.berzins@gmail.com'),
@@ -57,7 +60,21 @@ INSERT INTO Student_Profiles(student_id, address, phone_number)
 VALUES ('1', 'Riga', '12345678'),
 	   ('2', 'Ventspils', '3456789'),
 	   ('3', 'Sigulda', '213231253');
+       
+INSERT INTO Teachers(teacher_id, teacher_name, teacher_email)
+VALUES ('1', 'Olga', 'olga@example.com'),
+	   ('2', 'Inga', 'inga@example.com'),
+	   ('3', 'Gunita', 'gunita@example.com');
+	
+INSERT INTO Courses(course_id, course_name)
+VALUES ('1', 'programming'),
+('2', 'math'),
+('3', 'biology'),
+('4', 'english'),
+('5', 'history');
 
 SELECT s.student_id, s.student_name, s.student_email,  p.address, p.phone_number
 FROM Students s
 JOin Student_Profiles p ON s.student_id = p.student_id;
+
+SELECT
